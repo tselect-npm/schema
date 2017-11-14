dcd28c12b3f36ac8390fcfb25139d3619a2f7f9e
Nicolas Gramlich <nicolas.gramlich@volvocars.com>
2017-11-13 19:04:58 -0800

    :wrench: Making use of toStringRegExp.

--- src/factories/uuid.ts
+++ src/factories/uuid.ts
@@ -1,9 +1,10 @@
 import { TOptions } from '../types/options';
 import { TStringJSONSchema } from '../types/string-json-schema';
 import { string } from './string';
+import { toStringRegExp } from '../utils/to-string-reg-exp';
 
-const UUID_PATTERN: string = '^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$';
+const UUID_PATTERN: RegExp = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
 
 export function uuid(options: TOptions<TStringJSONSchema> = {}): TStringJSONSchema {
-  return string(Object.assign(options, { pattern: UUID_PATTERN }));
+  return string(Object.assign(options, { pattern: toStringRegExp(UUID_PATTERN) }));
 }
