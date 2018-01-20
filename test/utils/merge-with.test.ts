5745a7cde47fcc00a05bba6244050c410834d37e
Nicolas Gramlich <nicolas.gramlich@volvocars.com>
2018-01-19 16:40:33 -0800

    :rotating_light: Added test for mergeWith.

---
+++ test/utils/merge-with.test.ts
@@ -0,0 +1,29 @@
+import * as Schema from '../../';
+
+describe('mergeWith()', function () {
+  it('should return a merged object schema', () => {
+    const schema = Schema.object({
+      foo: Schema.string()
+    }, {
+      required: ['foo'],
+      additionalProperties: true
+    });
+
+    const merged = Schema.mergeWith(schema, Schema.object({
+      bar: Schema.string()
+    }, {
+      additionalProperties: true,
+      nullable: true
+    }));
+
+    expect(merged).to.deep.equal({
+      type: ['object', 'null'],
+      required: ['foo'],
+      additionalProperties: true,
+      properties: {
+        foo: { type: 'string' },
+        bar: { type: 'string' }
+      }
+    });
+  });
+});
