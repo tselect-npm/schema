53a9dac216d8849381d354b16ee0c285a23e2e24
Nicolas Gramlich <nicolas.gramlich@volvocars.com>
2020-09-15 14:21:29 -0700

    :rotating_light: Added failing test case.

--- test/utils/pick-properties.test.ts
+++ test/utils/pick-properties.test.ts
@@ -3,11 +3,64 @@ import * as Schema from '../../src/';
 
 describe('pickProperties()', function () {
   it('should return a cloned object schema with filtered properties', () => {
-    const schema = Schema.object({ foo: Schema.string(), bar: Schema.string() }, {
+    type TType = {
+      foo: string,
+      bar: string
+    };
+
+    const schema = Schema.object<TType>({ foo: Schema.string(), bar: Schema.string() }, {
       required: ['foo'],
       additionalProperties: true
     });
+
     const modified = Schema.pickProperties(schema, ['bar']);
+
+    expect(modified).to.deep.equal({
+      type: 'object',
+      required: [],
+      additionalProperties: true,
+      properties: {
+        bar: { type: 'string' }
+      }
+    });
+  });
+
+  it('should allow for property types to include undefined', () => {
+    type TType = {
+      foo: string,
+      bar: string | undefined
+    };
+
+    const schema = Schema.object<TType>({ foo: Schema.string(), bar: Schema.string() }, {
+      required: ['foo'],
+      additionalProperties: true
+    });
+
+    const modified = Schema.pickProperties(schema, ['bar']);
+
+    expect(modified).to.deep.equal({
+      type: 'object',
+      required: [],
+      additionalProperties: true,
+      properties: {
+        bar: { type: 'string' }
+      }
+    });
+  });
+
+  it('should allow for types to have optional properties', () => {
+    type TType = {
+      foo: string,
+      bar?: string | null
+    };
+
+    const schema = Schema.object<TType>({ foo: Schema.string(), bar: Schema.string() }, {
+      required: ['foo'],
+      additionalProperties: true
+    });
+
+    const modified = Schema.pickProperties(schema, ['bar']);
+
     expect(modified).to.deep.equal({
       type: 'object',
       required: [],
