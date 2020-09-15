08dcbb541e2d5c4546065cc2b870b97af4d74df6
Nicolas Gramlich <nicolas.gramlich@volvocars.com>
2020-09-15 14:22:02 -0700

    :bug: Fixed failing test case.

--- src/types/object-json-schema.ts
+++ src/types/object-json-schema.ts
@@ -2,7 +2,7 @@ import { TJSONSchema } from './json-schema';
 import { TNumberJSONSchema } from './number-json-schema';
 import { TStringJSONSchema } from './string-json-schema';
 
-type TProperties<T extends { [key: string]: unknown }, key extends keyof T> = T[key] extends number ? TNumberJSONSchema : T[key] extends string ? TStringJSONSchema : TJSONSchema;
+type TProperties<T extends { [key: string]: unknown }, key extends keyof T> = undefined | (T[key] extends number ? TNumberJSONSchema : T[key] extends string ? TStringJSONSchema : TJSONSchema);
 
 export type TObjectJSONSchema<T extends { [key: string]: unknown } = any> = TJSONSchema & {
   maxProperties?: number;
