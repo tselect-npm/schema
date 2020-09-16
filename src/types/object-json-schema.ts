bf755b4c0ec08b94c6e99b30b5750d199d0fa9a3
Nicolas Gramlich <nicolas.gramlich@volvocars.com>
2020-09-16 16:30:25 -0700

    :wrench: Undo TObjectJSONSchema change.

--- src/types/object-json-schema.ts
+++ src/types/object-json-schema.ts
@@ -1,8 +1,4 @@
 import { TJSONSchema } from './json-schema';
-import { TNumberJSONSchema } from './number-json-schema';
-import { TStringJSONSchema } from './string-json-schema';
-
-type TProperties<T extends { [key: string]: unknown }, key extends keyof T> = undefined | (T[key] extends number ? TNumberJSONSchema : T[key] extends string ? TStringJSONSchema : TJSONSchema);
 
 export type TObjectJSONSchema<T extends { [key: string]: unknown } = any> = TJSONSchema & {
   maxProperties?: number;
@@ -11,7 +7,7 @@ export type TObjectJSONSchema<T extends { [key: string]: unknown } = any> = TJSO
   additionalProperties?: boolean | TJSONSchema;
   definitions?: { [key: string]: TJSONSchema };
   properties?: {
-    [key in keyof T]: TProperties<T, key>;
+    [key in keyof T]?: TJSONSchema;
   };
   patternProperties?: { [pattern: string]: TJSONSchema };
   dependencies?: { [key: string]: TJSONSchema | string[] };
