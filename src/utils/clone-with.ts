f448fdb0ecbf759a6c4aecb66326853c7e705278
Sylvain Estevez <sylvain@luxe.com>
2020-10-28 07:53:54 +0100

    :shirt: Cleanup

--- src/utils/clone-with.ts
+++ src/utils/clone-with.ts
@@ -2,7 +2,12 @@ import { TObjectJSONSchema } from '..';
 import { TJSONSchema } from '../types/json-schema';
 import { clone } from './clone';
 
-type TCloneWithReturn<O extends (T extends TObjectJSONSchema ? TObjectJSONSchema : Partial<T>), T extends TJSONSchema = TJSONSchema> = O extends TObjectJSONSchema<infer U> ? TObjectJSONSchema<U> & Omit<T, 'required' | 'properties'> : T & O;
+type TCloneWithReturn<
+  O extends (T extends TObjectJSONSchema ? TObjectJSONSchema : Partial<T>),
+  T extends TJSONSchema = TJSONSchema
+> = O extends TObjectJSONSchema<infer U>
+  ? TObjectJSONSchema<U> & Omit<T, 'required' | 'properties'>
+  : T & O;
 
 export function cloneWith<T extends TJSONSchema = TJSONSchema, O extends (T extends TObjectJSONSchema ? TObjectJSONSchema : Partial<T>) = (T extends TObjectJSONSchema ? TObjectJSONSchema : Partial<T>)>(schema: T, overrides: O): TCloneWithReturn<O, T> {
   return Object.assign(clone(schema), overrides) as TCloneWithReturn<O, T>;
--- src/utils/merge-with.ts
+++ src/utils/merge-with.ts
@@ -3,7 +3,13 @@ import { TObjectJSONSchema } from '..';
 import { TJSONSchema } from '../types/json-schema';
 import { clone } from './clone';
 
-type TMergeWithReturn<O extends (T extends TObjectJSONSchema ? TObjectJSONSchema : Partial<T>), T extends TJSONSchema = TJSONSchema> = O extends TObjectJSONSchema<infer U> ? T extends TObjectJSONSchema<infer V> ? TObjectJSONSchema<U & V> & Omit<T, 'required' | 'properties'> : TObjectJSONSchema<U> & Omit<T, 'required' | 'properties'> : T & O;
+type TMergeWithReturn<
+  O extends (T extends TObjectJSONSchema ? TObjectJSONSchema : Partial<T>), T extends TJSONSchema = TJSONSchema
+> = O extends TObjectJSONSchema<infer U>
+  ? T extends TObjectJSONSchema<infer V>
+    ? TObjectJSONSchema<U & V> & Omit<T, 'required' | 'properties'>
+    : TObjectJSONSchema<U> & Omit<T, 'required' | 'properties'>
+  : T & O;
 
 export function mergeWith<T extends TJSONSchema = TJSONSchema, O extends (T extends TObjectJSONSchema ? TObjectJSONSchema : Partial<T>) = (T extends TObjectJSONSchema ? TObjectJSONSchema : Partial<T>)>(schema: T, overrides: O): TMergeWithReturn<O, T> {
   return Lodash.mergeWith(clone(schema), overrides, (obj: any, src: any) => {
